3bfd8e2756faf3af51320cacf4357e1f26ffee6a
Marcos Canales Mayo <marketes94@gmail.com>
2016-12-30 17:44:21 +0100

    comp and run script

--- comp_run.sh
+++ comp_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# 1st param: MPI -n
+
+# 1st param: MPI -n and first C programa parameter
 # 2nd param: MPI --hostfile
 # 3rd param: {file}.c
-# Remaining params: C program parameters
+# Remaining params: C remaining program parameters
 echo "mpicc $3.c -o $3.o -lm"
 mpicc $3.c -o $3.o -lm
 echo "mpirun -n $1 --hostfile $2 $3.o $1 ${@:4}"
