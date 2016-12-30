a574150ac642d1a492895e5078faeb1c6df22767
Marcos Canales <Marcos Canales>
2016-12-30 18:45:20 +0100

    typo

--- comp_run.sh
+++ comp_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 1st param: MPI -n and first C program parameter
+# 1st param: MPI -n and C program first parameter
 # 2nd param: MPI --hostfile
 # 3rd param: {file}.c
-# Remaining params: C remaining program parameters
+# Remaining params: C program remaining parameters
 echo "mpicc $3.c -o $3.o -lm"
 mpicc $3.c -o $3.o -lm
 echo "mpirun -n $1 --hostfile $2 $3.o $1 ${@:4}"
