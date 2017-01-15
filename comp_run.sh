de116d0e3f168ba9ae718188266ba7f134d9d35c
Marcos Canales Mayo <marketes94@gmail.com>
2017-01-15 23:52:42 +0100

    updated to local mpi version

--- comp_run.sh
+++ comp_run.sh
@@ -5,7 +5,7 @@
 # 3rd param: {file}.c
 # 4th param: OMP_NUM_THREADS
 # Remaining params: C program remaining parameters
-echo "mpicc -fopenmp $3.c -o $3.o -lm"
-mpicc -fopenmp $3.c -o $3.o -lm
-echo "mpirun -n $1 --hostfile $2 $3.o $4 $1 ${@:5}"
-mpirun -n $1 --hostfile $2 $3.o $4 $1 ${@:5}
+echo "/usr/local/openmpi/bin/mpicc -fopenmp $3.c -o $3.o -lm"
+/usr/local/openmpi/bin/mpicc -fopenmp $3.c -o $3.o -lm
+echo "/usr/local/openmpi/bin/mpirun -n $1 --hostfile $2 $3.o $4 $1 ${@:5}"
+/usr/local/openmpi/bin/mpirun -n $1 --hostfile $2 $3.o $4 $1 ${@:5}
